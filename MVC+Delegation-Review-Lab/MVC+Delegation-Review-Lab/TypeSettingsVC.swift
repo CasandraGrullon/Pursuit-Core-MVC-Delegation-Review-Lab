eed9d1282679b851290207abbc24bdb02f9eab3a
Casandra <casandragrullon@pursuit.org>
2019-11-20 17:50:59 -0500

    got both slider and stepper to sync

--- MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/TypeSettingsVC.swift
+++ MVC+Delegation-Review-Lab/MVC+Delegation-Review-Lab/TypeSettingsVC.swift
@@ -14,37 +14,52 @@ class TypeSettingsVC: UIViewController {
     @IBOutlet weak var fontSizeLabel: UILabel!
     @IBOutlet weak var stepper: UIStepper!
     
+    
+    var stepperFont: Double = 17.0 {
+        didSet{
+            let value = String(format: "%0.f", stepper.value)
+            fontSizeLabel.text = "Preview Font Size: \(value)"
+        }
+    }
+    
+    var sliderFont: Float = 17.0 {
+        didSet {
+            let value = String(format: "%0.f", slider.value)
+            fontSizeLabel.text = "Preview Font Size: \(value)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureSlider()
         configureStepper()
     }
 
-    var currentTextSizeSlider: Float = 17 {
-        didSet{
-            fontSizeLabel.text = "Preview Font Size: \(slider.value)"
-        }
-    }
+
     func configureSlider() {
-        slider.minimumValue = 6
-        slider.maximumValue = 40
-        slider.value = 17
+        slider.minimumValue = 8.0
+        slider.maximumValue = 40.0
+        slider.value = 17.0
     }
     
     func configureStepper() {
-        stepper.minimumValue = Double(slider.minimumValue)
-        stepper.maximumValue = Double(slider.maximumValue)
-        stepper.value = Double(slider.value)
+        stepper.minimumValue = 8.0
+        stepper.maximumValue = 40.0
+        stepper.value = 17.0
+        stepper.stepValue = 1.0
     }
     
     
     @IBAction func sliderAction(_ sender: UISlider) {
-        currentTextSizeSlider = sender.value
+        sliderFont = sender.value
+        stepper.value = Double(sender.value)
+        
     }
     
     
     @IBAction func stepperAction(_ sender: UIStepper) {
-        currentTextSizeSlider = sender.value
+        stepperFont = sender.value
+        slider.value = Float(sender.value)
     }
     
     
